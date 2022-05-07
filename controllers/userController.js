fe29e24437a55002ed466c442e5b2b814227cc86
tnoonan <tnoonan88@gmail.com>
2022-05-07 15:17:01 -0400

    debugged get all users route

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const userController = {
     getAllUsers(req, res) {
         User.find()
             .populate({
-                path: 'thought',
+                path: 'thoughts',
                 select: '-__v'
             })
             .select('-__v')
